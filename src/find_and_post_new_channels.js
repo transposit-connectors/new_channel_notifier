656970c8b1b9dfe5ca73f7922df9b2159ac5c7c7
Dan Moore <mooreds@gmail.com>
2019-10-18 17:29:40 +0000

    first working variation

---
+++ src/find_and_post_new_channels.js
@@ -0,0 +1,29 @@
+(params) => {
+  const list = api.run("this.get_channels_list");
+  const new_channels = [];
+  list.forEach((e) => {
+    if (!stash.get(e.id)) {
+      new_channels.push(e.id);
+      stash.put(e.id,true);  
+    }
+  });
+  
+  if (new_channels.length > 0) {
+  const new_channel_announcement_channel = 'test';
+  const formatted_ids = new_channels.map((id) => {
+     return "<#"+id+">";
+  }).join(", ").replace(",$","");
+  // console.log(formatted_ids);
+  
+  api.run("this.post_chat_message",{
+    new_channel_announcement_channel: new_channel_announcement_channel,
+    list_of_formatted_channel_ids: formatted_ids
+  });
+  }
+
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
